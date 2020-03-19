b72118cf72d23c866cc5ef506b0c7b105be09ebe
Thibaut <thithan77@gmail.com>
2020-03-19 20:21:04 +0100

    Ajout du système pour casser les blocs.

--- init.py
+++ init.py
@@ -1,8 +1,21 @@
 import pygame
+from math import *
+from random import *
 sol = pygame.image.load("assets/sprite_016.png").convert()
 pierre = pygame.image.load("assets/sprite_007.png").convert()
 vide = pygame.image.load("assets/sprite_186.png").convert()
-map = [0]*100
-for i in range(99):
-    map[i] = "ssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
-print(len(map))
+arbre = pygame.image.load("assets/arbre.png").convert_alpha()
+cailloux = pygame.image.load("assets/cailloux.png").convert_alpha()
+map = [0]*1000
+surmap = [0]*1000
+for i in range(999):
+    map[i] = "ssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssmmmmssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+for i in range(999):
+    surmap[i] = [0]*1000
+    for j in range(999):
+        surmap[i][j] = "v"
+        if(floor(random()*30) == 0):
+            surmap[i][j] = "a"
+        elif(floor(random()*30) == 0):
+            surmap[i][j] = "c"
+inv = [0]*10
--- main.py
+++ main.py
@@ -9,12 +9,15 @@ from perso import *
 from math import *
 Miguel = Perso(fen)
 lastTick = int(pygame.time.get_ticks())
+destroyingTime = 0
+destroying = False
+destroyingPlace = (0,0)
 font=pygame.font.Font(None, 24)
 while cont:
-    xmin = Miguel.pos["x"] - (width/2)
-    xmax = Miguel.pos["x"] + (width/2)
-    ymin = Miguel.pos["y"] - (height/2)
-    ymax = Miguel.pos["y"] + (height/2)
+    xmin = Miguel.pos["x"] - (width/2) +32
+    xmax = Miguel.pos["x"] + (width/2) +32
+    ymin = Miguel.pos["y"] - (height/2) +32
+    ymax = Miguel.pos["y"] + (height/2) +32
     x = xmin
     y = ymin
     for i in range(26):
@@ -27,6 +30,12 @@ while cont:
                 fen.blit(sol,(j*32-xc,i*32-yc))
             else:
                 fen.blit(pierre,(j*32-xc,i*32-yc))
+            if(floor(x/32) <= 0 or floor(y/32) <= 0):
+                fen.blit(vide,(j*32-xc,i*32-yc))
+            elif(surmap[floor(y/32)-1][floor(x/32)-1] == "a"):
+                fen.blit(arbre,((j)*32-xc,(i)*32-yc))
+            elif(surmap[floor(y/32)-1][floor(x/32)-1] == "c"):
+                fen.blit(cailloux,((j)*32-xc,(i)*32-yc))
             x+=32
         x = xmin
         y+=32
@@ -46,15 +55,55 @@ while cont:
             elif event.key == K_f:
                 print("xmin:"+str(xmin))
                 print("xmax:"+str(xmax))
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                destroying = True
+                destroyingPlace = (event.pos[0],event.pos[1])
+                print(destroyingPlace)
+        elif event.type == MOUSEBUTTONUP:
+            if event.button == 1:
+                destroying = False
+                destroyingTime = 0
     time = int(pygame.time.get_ticks())-lastTick
+    if(destroying):
+        destroyingTime+=(int(pygame.time.get_ticks())-lastTick)
+        x = destroyingPlace[0] + (Miguel.pos["x"] - (width/2))
+        y = destroyingPlace[1] + (Miguel.pos["y"] - (height/2))
+        print(surmap[floor(y/32)][floor(x/32)])
+        if(surmap[floor(y/32)][floor(x/32)] == "a" and destroyingTime > 4000):
+            xM = floor(Miguel.pos["x"]/32)
+            yM = floor(Miguel.pos["y"]/32)
+            xO = floor(x/32)
+            yO = floor(y/32)
+            d = sqrt((xO-xM)*(xO-xM)+(yO-yM)*(yO-yM))
+            if(d < 4):
+                surmap[floor(y/32)][floor(x/32)] = "v"
+                inv[0]+=1;
+        elif(surmap[floor(y/32)][floor(x/32)] == "c" and destroyingTime > 4000):
+            xM = floor(Miguel.pos["x"]/32)
+            yM = floor(Miguel.pos["y"]/32)
+            xO = floor(x/32)
+            yO = floor(y/32)
+            d = sqrt((xO-xM)*(xO-xM)+(yO-yM)*(yO-yM))
+            if(d < 4):
+                surmap[floor(y/32)][floor(x/32)] = "v"
+                inv[1]+=1;
     FPS = floor(1/(time*0.001));
-    text = font.render("FPS:"+str(FPS),1,(255,255,255))
+    text = font.render("GAMEPLAY EXPERIMENTAL:",1,(255,255,255))
     fen.blit(text,(0,0))
-    text = font.render("PosX:"+str(floor(Miguel.pos["x"]/32)),1,(255,255,255))
+    text = font.render("FPS:"+str(FPS),1,(255,255,255))
     fen.blit(text,(0,32))
-    text = font.render("PosY:"+str(floor(Miguel.pos["y"]/32)),1,(255,255,255))
+    text = font.render("PosX:"+str(floor(Miguel.pos["x"]/32)),1,(255,255,255))
     fen.blit(text,(0,64))
+    text = font.render("PosY:"+str(floor(Miguel.pos["y"]/32)),1,(255,255,255))
+    fen.blit(text,(0,96))
+    text = font.render("DestroyingTime:"+str(destroyingTime),1,(255,255,255))
+    fen.blit(text,(0,96+32))
     lastTick = int(pygame.time.get_ticks())
     Miguel.afficher()
+    pygame.draw.rect(fen,(0,0,150),((config.width-(10*32))/2,height-32,10*32,32))
+    for i in range(10):
+        text = font.render(str(inv[i]),1,(255,255,255))
+        fen.blit(text,((config.width-(10*32))/2+(i*32),height-32))
     pygame.display.flip()
 pygame.quit()
